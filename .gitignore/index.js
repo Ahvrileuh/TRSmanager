fccadc81a590bec21a201afcca7573479f4af36e
Ahvrileuh <40494366+Ahvrileuh@users.noreply.github.com>
2018-07-02 20:26:34 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,11 +1,12 @@
 const Discord = require('discord.js');
 const clientDiscord = new Discord.Client();
 
-var prefix = "";
+var prefix = "!";
 var role = "";
 
 clientDiscord.on('ready', ()=> {
-	clientDiscord.user.setPresence({ game: { name: '!cmd', type: 0}});
+    clientDiscord.user.setPresence({ game: { name: '!cmd', type: 0}});
+    console.log("JOSSE is ready");
 });
 
 clientDiscord.login(process.env.TOKEN);
@@ -14,7 +15,7 @@ clientDiscord.login(process.env.TOKEN);
   	
 		const guildMember = message.member;
     		
-  		if(message.content === prefix + "add unity " && message.channel.name == 'roles-manager'){
+  		if(message.content === prefix + "add unity" && message.channel.name == 'roles-manager'){
 			role = message.guild.roles.find('name', 'UNITY');
 			guildMember.addRole(role);
 			message.channel.send("role **UNITY** added !");
